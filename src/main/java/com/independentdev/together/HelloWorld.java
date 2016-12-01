e1cd434353e14c00d00c3c18a4612408e2b2f24f
Sanjay DevHack <sanjaydevhack@gmail.com>
2016-12-01 09:12:53 +0530

    First Commit

---
+++ src/main/java/com/independentdev/together/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.independentdev.together;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created by lenovo on 27-Nov-16.
+ */
+@Path("/hello")
+public class HelloWorld {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getMessage() {
+        return "Hello world!";
+    }
+}
